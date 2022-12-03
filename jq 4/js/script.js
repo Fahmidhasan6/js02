b986529ff2640817ce568faf7230930c26ab0276
Fahmid Hasan <110227791+Fahmidhasan6@users.noreply.github.com>
2022-12-03 18:56:58 +0600

    Add files via upload

---
+++ jq 4/js/script.js
@@ -0,0 +1,14 @@
+
+$( function() {
+  $( "#draggable" ).draggable();
+  $( "#droppable" ).droppable({
+    drop: function( event, ui ) {
+      $( this )
+        .addClass( "ui-state-highlight" )
+        .find( "p" )
+          .html( "Dropped!" );
+    }
+  });
+} );
+
+
